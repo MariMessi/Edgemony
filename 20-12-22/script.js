637b0de4088e2ac625e7ab61bbd16074049d7d77
MariMessi <121039446+MariMessi@users.noreply.github.com>
2022-12-20 17:58:51 +0100

    Add files via upload

---
+++ 20-12-22/script.js
@@ -0,0 +1,32 @@
+//Esercizio 1
+let clothes = [];
+
+
+clothes.push(prompt("inserisci 10 indumenti"));
+console.log(clothes);
+ 
+
+
+
+for (let clothes = 0; clothes <=10; clothes++) {
+ console.log(clothes);
+}
+
+
+
+
+//Avanzato
+//let clothes = ["giacca", "cravatta", "camicia", "pantaloni", "gonna", "calzini", "maglietta", "felpa", "cappello", "guanti"];
+//console.log(clothes);
+
+//for in 
+//for (let clothesIndex in clothes) {
+//  console.log(clothesIndex);
+//}
+
+//for of
+ 
+//for (let clothes of clothes) {
+//   console.log(clothes);
+//}
+
