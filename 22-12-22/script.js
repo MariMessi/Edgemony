4734b37a79e6afec79d8027de80b275525e58505
MariMessi <121039446+MariMessi@users.noreply.github.com>
2022-12-22 17:58:31 +0100

    Add files via upload

---
+++ 22-12-22/script.js
@@ -0,0 +1,97 @@
+//Esercizio 
+//function oper() {
+//    let a = "19.939";
+//    let b = "4";
+//    let op = prompt("inserisci un'operazione");
+    
+     
+//    let result = a + b + op
+
+//    if (op === "+") {
+//        console.log(Math.floor(a) + b);
+//    }
+
+//    else if (op === "-") {
+//        console.log(Math.floor(a) - b);
+//    };
+
+//    if (op === "*") {
+//        console.log(Math.floor(a) * b);
+//    };
+
+//    if (op === "/") {
+//        console.log(Math.floor(a) / b);
+//    };
+
+//};
+
+//oper();
+
+
+
+//Avanzato 
+let prod1 = ["1",
+ "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+  "109.95", 
+  "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday", 
+  "men's clothing", 
+  "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg", 
+  "3.9", 
+  "120"];
+
+    
+let prod2 =   ["2", 
+ "Mens Casual Premium Slim Fit T-Shirts ",
+ "22.3", 
+ "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+ "men's clothing", 
+ "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg", 
+ "4.1",
+ "259"];
+
+    
+let prod3 = ["3",
+    "Mens Cotton Jacket",
+    "55.99",
+    "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+    "men's clothing",
+    "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    "4.7",
+    "500"];
+    
+    
+let prod4 = [ "4",
+    "Mens Casual Slim Fit",
+    "15.99",
+    "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+    "men's clothing",
+    "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    "2.1",
+    "430"]
+
+   
+let prod5 = ["5",
+    "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+    "695",
+    "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+    "jewelery",
+    "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+    "4.6",
+    "400"];
+
+
+    console.log(prod1[1], prod2[1], prod3[1], prod4[1], prod5[1]);
+    console.log(prod1[2], prod2[2], prod3[2], prod4[2], prod5[2]);
+    console.log(prod1[7], prod2[7], prod3[7], prod4[7], prod5[7]);
+
+
+
+
+
+
+
+
+
+
+
+
