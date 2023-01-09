8260a9a21b00fa257438be4337b22869f524398b
MariMessi <121039446+MariMessi@users.noreply.github.com>
2023-01-09 17:51:12 +0100

    Add files via upload

---
+++ 09-01-23/script.js
@@ -0,0 +1,46 @@
+
+// L'utente deve inserire solo l'operazione
+//const calculator = (num1, num2) => 100 + 50;
+
+ //let operation = prompt("Scegli un'operazione matematica");
+
+  // if (operation === "+") {
+   //     console.log(100 + 50);
+   //   } else if (operation === "-") {
+   //     console.log(100 - 50);
+   //   } else if (operation === "*") {
+   //     console.log(100 * 50);
+   //  } else if (operation === "/") {
+   //   console.log(100 / 50);
+  //}
+    
+
+
+
+  //L'utente deve inserire sia i numeri che l'operazione
+//const calculator2 = (numOne, numTwo) => numO, 
+//numOne = prompt ("inserisci un numero");
+//numTwo = prompt ("inserisci un altro numero");
+
+
+//let operation = prompt("scegli un'operazione")
+
+//if (operation === "+") {
+ //  console.log(numOne + numTwo);
+ //        } else if (operation === "-") {
+ //         console.log(numOne - numTwo);
+ //   } else if (operation === "*") {
+ //          console.log(numOne * numTwo);
+ //        } else if (operation === "/") {
+ //          console.log;(numOne / numTwo)
+ //        }
+
+      // Avanzato
+
+      const calculator = (num1, num2) => 100 + 50;
+      const operation = (num3, num4) => {
+        num3 = "12";
+        num4 = "6";
+        return (num3 / num4);
+      }
+console.log(operation(calculator(100, 50)));
