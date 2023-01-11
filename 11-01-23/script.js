891e1990071d4713efefec1c028b01e02a78c860
MariMessi <121039446+MariMessi@users.noreply.github.com>
2023-01-11 17:56:20 +0100

    Add files via upload

---
+++ 11-01-23/script.js
@@ -0,0 +1,25 @@
+
+
+////////// Al click del mouse cambia il colore del testo //////////
+function colorChange () {
+    const h1 = document.querySelector(".h1");
+    const p = document.querySelector("p");
+
+    h1.style.color = '#483d8b';
+    p.style.color = '#f08080';
+}
+
+const color = 
+document.querySelector(".card")
+document.addEventListener("click", colorChange);
+
+////////// Al click del mouse la card si elimina //////////
+//let btnPush = 1;
+//const Btn = document.querySelector ('btn');
+//if (btnPush <= 2) {
+//    document.addEventListener("click", () => {
+//        const card = document.querySelector("card");
+//        card.style.display = "none";
+//    })
+//}
+
