f9b4f0c64002fb284988189546949c29848409db
MariMessi <121039446+MariMessi@users.noreply.github.com>
2023-03-31 16:50:08 +0200

    Delete Le Tour/src directory

--- Le Tour/src/utils/http.js
+++
@@ -1,7 +0,0 @@
-const BASE_URL = "https://failteireland.azure-api.net/opendata-api/v1/";
-
-export const GET = async (query) => {
-    const res = await fetch(`${BASE_URL} ${query}`);
-    const data = await res.json();
-    return data;
-};
